9745b915a7c24a8b6de6f93881f4120abd34d45c
Robin Ochieng <robinochieng73@gmail.com>
2025-05-26 08:09:21 +0300

    Updated this module

--- modules/module8Module.R
+++ modules/module8Module.R
@@ -1,8 +1,16 @@
-# This module is for the IFRS 17 module 8
+# This module is for the IFRS 17 module 9
 IFRS17Module8UI <- function(id) {
   ns <- NS(id)
     tagList(
-    h2("Module 3"),
+    h2("Module 8: Onerous Contracts", class = "section-title"),
+    div(
+      class = "quiz-nav",
+      actionButton(
+          ns("to_module_9"),
+          label = tagList(icon("arrow-right"), "Next: Module 9 - Premium Allocation Approach"),
+          class = "control-button-tavnav"
+      )
+    ) 
     )
 }
 
@@ -10,5 +18,9 @@ IFRS17Module8Server <- (function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
+    # create a reactive for the “Next” click
+    to_module_9 <- reactive(input$to_module_9)
+    # return it so the app can observe it
+    to_module_9
   })
 })
